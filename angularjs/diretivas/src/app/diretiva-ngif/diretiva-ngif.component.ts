91f4adfe2ce4242d3a22408305b486eaf0326f4c
Gerson Prado <gecozzy@hotmail.com>
2019-03-08 00:28:12 -0300

    Diretivas

--- angularjs/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ angularjs/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
+  cursos:string[] = ["Curso de Angular"] ;
+  mostrarCursos: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
   }
 
 }
