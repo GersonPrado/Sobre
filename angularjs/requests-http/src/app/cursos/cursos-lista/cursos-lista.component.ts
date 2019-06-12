859c1d054bff87d5d01a0f014008218ea1fc6d3b
Gerson Prado <gecozzy@hotmail.com>
2019-06-11 23:32:05 -0300

    Requests http - Rest

---
+++ angularjs/requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject, empty } from 'rxjs';
+
+import { CursosService } from '../cursos.service';
+import { Curso } from './curso';
+import { catchError } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-cursos-lista',
+  templateUrl: './cursos-lista.component.html',
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
+})
+export class CursosListaComponent implements OnInit {
+  cursos: Curso[];
+  cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
+
+  constructor(private service: CursosService) { }
+
+  ngOnInit() {
+    /* this.service.list().subscribe(dados => this.cursos = dados);*/
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        this.error$.next(true);
+        return empty();
+      })
+    );
+  }
+
+}
