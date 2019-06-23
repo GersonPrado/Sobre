7b1ea9e5d40aa3ff8780a705079c1a1c0a7b0680
Gerson Prado <gecozzy@hotmail.com>
2019-06-23 20:30:31 -0300

    Https_request

--- angularjs/requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ angularjs/requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,9 +1,11 @@
+import { AlertModalService } from './../../shared/alert-modal.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject, empty, EMPTY } from 'rxjs';
+import { Observable, Subject, EMPTY } from 'rxjs';
 
 import { CursosService } from '../cursos.service';
 import { Curso } from './curso';
 import { catchError } from 'rxjs/operators';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -12,21 +14,32 @@ import { catchError } from 'rxjs/operators';
   preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
+  // bsModalRef: BsModalRef;
   cursos: Curso[];
   cursos$: Observable<Curso[]>;
   error$ = new Subject<boolean>();
 
-  constructor(private service: CursosService) { }
+  constructor(private service: CursosService,
+              private  alertService: AlertModalService
+    ) { }
 
   ngOnInit() {
     /* this.service.list().subscribe(dados => this.cursos = dados);*/
     this.cursos$ = this.service.list()
     .pipe(
       catchError(error => {
-        this.error$.next(true);
+        // this.error$.next(true);
+        this.handleError();
         return EMPTY;
       })
     );
-  }
 
+  }
+  handleError() {
+    this.alertService.showAlertDanger('Erro ao carregar cursos  ');
+    /* this.bsModalRef = this.modalService.show(AlertModalComponent);
+      this.bsModalRef.content.type = 'danger';
+      this.bsModalRef.content.message = 'Erro ao carregar os cursos !';
+    */
+  }
 }
